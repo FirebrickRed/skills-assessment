631f2aaa1fb02cb9317c6280515222f4c939bb1b
Jessica <jbjabzin@gmail.com>
2021-03-02 11:40:00 -0700

    Added Flipping between phone and email

--- src/index.js
+++ src/index.js
@@ -5,15 +5,18 @@ contacts.forEach(person => {
   newEntry.setAttribute('class', 'entry');
 
   let nameLabel = document.createElement('div');
+  nameLabel.setAttribute('class', 'name');
   nameLabel.innerHTML = person.name;
 
   let dot = document.createElement('div');
   dot.setAttribute('class', person.dot);
 
   let email = document.createElement('div');
+  email.setAttribute('class', 'email');
   email.innerHTML = person.email;
 
   let phone = document.createElement('div');
+  phone.setAttribute('class', 'phone');
   phone.innerHTML = person.phoneNumber;
 
   newEntry.appendChild(dot);
@@ -23,3 +26,20 @@ contacts.forEach(person => {
 
   contactslistDOM.appendChild(newEntry);
 });
+
+let isEmail = true;
+document.getElementById('sorting').addEventListener('change', event => {
+  let email = document.querySelectorAll(".email");
+  let phone = document.querySelectorAll(".phone");
+
+  console.log(phone);
+  
+  isEmail = !isEmail;
+  if(isEmail) {
+    email.forEach(e => e.style.display = 'block');
+    phone.forEach(p => p.style.display = 'none');
+  } else {
+    phone.forEach(p => p.style.display = 'block');
+    email.forEach(e => e.style.display = 'none');
+  }
+})
